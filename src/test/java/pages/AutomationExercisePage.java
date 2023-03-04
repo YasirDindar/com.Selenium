c083dd336dad21a9c9d7347a8f4b8744115c2343
YasirDindar <myasirdindar@gmail.com>
2023-03-04 11:19:36 +0300

    Exercise

---
+++ src/test/java/pages/AutomationExercisePage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AutomationExercisePage {
+    public AutomationExercisePage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy (xpath = "//i[@class='fa fa-lock']")
+    public WebElement sugUpLinki;
+
+
+    @FindBy (xpath = "//input[@name='name']")
+    public WebElement isimKutusu;
+
+
+    @FindBy (xpath = "(//input[@name='email'])[2]")
+    public WebElement passwordKutusu;
+
+    @FindBy (xpath = "(//button[@type='submit'])[2]")
+    public WebElement sigUpButonu;
+
+
+    @FindBy (xpath = "(//input[@type='radio'])[1]")
+    public WebElement mrRadioButonu;
+
+    @FindBy (xpath = "//button[text()='Create Account']")
+    public WebElement createAccountButonu;
+
+
+    @FindBy (xpath = "//b[text()='Account Created!']")
+    public WebElement accountCreatedYaziElementi;
+}
